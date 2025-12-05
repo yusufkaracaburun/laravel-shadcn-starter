dd0ba11720b6e3d0017a3a681efdccc3bf07fe81
yusufkaracaburun <yusuf.karacaburun@grandvision.com>
2025-12-05 09:52:54 +0100

    users feature

--- api/app/Http/Resources/BaseCollection.php
+++ api/app/Http/Resources/BaseCollection.php
@@ -25,6 +25,11 @@ abstract class BaseCollection extends ResourceCollection
             'total'         => $this->total(),
             'from'          => $this->firstItem(),
             'to'            => $this->lastItem(),
+            'path'          => $this->path(),
+            'first_page_url' => $this->url(1),
+            'last_page_url'  => $this->url($this->lastPage()),
+            'next_page_url'  => $this->nextPageUrl(),
+            'prev_page_url'  => $this->previousPageUrl(),
         ];
     }
 }
