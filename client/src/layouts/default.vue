033c130eb9eaf6305ddb95232876096315b9eef7
yusufkaracaburun <yusuf.karacaburun@grandvision.com>
2025-12-07 00:20:52 +0100

    updates

--- client/src/layouts/default.vue
+++ client/src/layouts/default.vue
@@ -13,7 +13,7 @@ const themeStore = useThemeStore()
 const { contentLayout } = storeToRefs(themeStore)
 </script>
 
-<template>
+<!-- <template>
   <UiSidebarProvider :default-open="defaultOpen.get('sidebar:state')">
     <AppSidebar />
     <UiSidebarInset
@@ -23,6 +23,29 @@ const { contentLayout } = storeToRefs(themeStore)
 
       <div :class="cn('p-4 grow', contentLayout === 'centered' ? 'container mx-auto ' : '')">
         <router-view />
+      </div>
+    </UiSidebarInset>
+  </UiSidebarProvider>
+</template> -->
+
+<template>
+  <UiSidebarProvider
+    :default-open="defaultOpen.get('sidebar:state')" :style="{
+      '--sidebar-width': 'calc(var(--spacing) * 72)',
+      '--header-height': 'calc(var(--spacing) * 12)',
+    }"
+  >
+    <AppSidebar variant="inset" />
+    <UiSidebarInset>
+      <Header />
+      <div class="flex flex-1 flex-col">
+        <div class="@container/main flex flex-1 flex-col gap-2">
+          <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div class="px-4 lg:px-6">
+              <router-view />
+            </div>
+          </div>
+        </div>
       </div>
     </UiSidebarInset>
   </UiSidebarProvider>
