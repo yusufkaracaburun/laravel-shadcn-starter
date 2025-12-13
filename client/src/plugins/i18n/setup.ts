086e28a6c3ffb4d1b927e0c5611fb70980bb703a
yusufkaracaburun <yusuf.karacaburun@grandvision.com>
2025-12-13 17:46:15 +0100

    updates

--- client/src/plugins/i18n/setup.ts
+++ client/src/plugins/i18n/setup.ts
@@ -3,16 +3,18 @@ import type { App } from 'vue'
 import { createI18n } from 'vue-i18n'
 
 import en from './en.json'
+import nl from './nl.json'
 import zh from './zh.json'
 
 export function setupI18n(app: App) {
   const i18n = createI18n({
     legacy: false,
-    locale: 'en',
+    locale: 'nl',
     fallbackLocale: 'en',
     messages: {
-      zh,
       en,
+      nl,
+      zh,
     },
   })
   app.use(i18n)
