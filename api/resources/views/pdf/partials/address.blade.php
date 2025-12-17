53ac394406c66d35cf807b6ee49d0584943181be
yusufkaracaburun <yusuf.karacaburun@grandvision.com>
2025-12-17 18:51:28 +0100

    updates

--- api/resources/views/pdf/partials/address.blade.php
+++ api/resources/views/pdf/partials/address.blade.php
@@ -1,6 +1,6 @@
 {{-- KLANT + FACTUUR INFO --}}
 <div class="address">
-    <table class="mb-20">
+    <table>
         <tr>
             <td class="customer" width="50%">
                 <h2>Klant</h2>
@@ -9,8 +9,8 @@
                 {{ $data->customer->zipcode }} {{ $data->customer->city }}
             </td>
 
-            <td width="50%" class="invoice-info text-right">
-                <strong>Factuur</strong><br>
+            <td width="50%" class="invoice-info">
+                <h2>Factuur</h2>
                 Factuur #{{ $data->invoice_number }}<br>
                 Datum {{ $data->date->format('d-m-Y') }}<br>
                 Vervaldatum {{ $data->date_due->format('d-m-Y') }}
