5bd7415968a6871bee42665334bfa6f0314fb07a
yusufkaracaburun <yusuf.karacaburun@grandvision.com>
2026-01-08 14:34:04 +0100

    updates

--- client/eslint.config.app.mjs
+++ client/eslint.config.app.mjs
@@ -12,6 +12,7 @@ export default {
       'before',
       {
         overrides: {
+          '=': 'after',
           '?': 'before',
           ':': 'before',
           '&&': 'before',
@@ -25,6 +26,7 @@ export default {
       'before',
       {
         overrides: {
+          '=': 'after',
           '?': 'before',
           ':': 'before',
           '&&': 'before',
